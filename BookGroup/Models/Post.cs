3d4602820111f6afcef44eac3cb7805209ba28cc
norogoth <norogoth@gmail.com>
2023-01-29 06:07:06 -0800

    Add project files.

---
+++ BookGroup/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace BookGroup.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int CreatorUserId { get; set; }
+        public string Quote { get; set; }
+        public string Note { get; set; }
+        public override string ToString() => JsonSerializer.Serialize<Post>(this);
+    }
+}
---
+++ BookGroup/Services/JsonFilePostsService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BookGroup.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace BookGroup.Services
+{
+    public class JsonFilePostsService
+    {
+        public JsonFilePostsService (IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "posts.json"); }
+        }
+        public IEnumerable<Post> GetPosts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+            return JsonSerializer.Deserialize<Post[]>(jsonFileReader.ReadToEnd(),
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+            }
+        }
+    }
+}
