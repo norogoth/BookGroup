4c10d993fee145243184723ccfbbebfeb997c997
norogoth <norogoth@gmail.com>
2023-01-29 13:28:06 -0800

    fixed the modal

--- BookGroup/Models/Post.cs
+++ BookGroup/Models/Post.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 namespace BookGroup.Models
@@ -9,6 +10,7 @@ namespace BookGroup.Models
     public class Post
     {
         public int Id { get; set; }
+        [Required]
         public int CreatorUserId { get; set; }
         public string Quote { get; set; }
         public string Note { get; set; }
