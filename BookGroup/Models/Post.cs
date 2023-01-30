3eb69244529549537c60b93908909cbb7bfa90cd
norogoth <norogoth@gmail.com>
2023-01-30 11:39:35 -0800

    may abandon this editform method

--- BookGroup/Models/Post.cs
+++ BookGroup/Models/Post.cs
@@ -12,7 +12,10 @@ namespace BookGroup.Models
         public int Id { get; set; }
         [Required]
         public int CreatorUserId { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "Please give me something longer than that UwU.")]
         public string Quote { get; set; }
+        public string QuoteLocation { get; set; }
         public string Note { get; set; }
         public override string ToString() => JsonSerializer.Serialize<Post>(this);
     }
